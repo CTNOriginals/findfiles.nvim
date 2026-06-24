53081aab378657dc21081d068da668fcbb48f41a
CTNOriginals <info@ctn-originals.com>
2026-06-24 21:37:05 +0200

    wip(core): config and setup

---
+++ lua/findfiles/config.lua
@@ -0,0 +1,11 @@
+---@class findfilesConfig
+---@field key string
+
+local M = {}
+
+---@type findfilesConfig
+M.defaults = {
+	key = "<leader>sF",
+}
+
+return M
--- lua/findfiles/init.lua
+++ lua/findfiles/init.lua
@@ -1,6 +1,12 @@
+M = {}
+
 require("findfiles.state")
 local ui = require("findfiles.ui")
+
+M.setup = function() end
 
 vim.keymap.set("n", "<leader>f", ui.createWindow, {
 	desc = "[f]ind files",
 })
+
+return M
